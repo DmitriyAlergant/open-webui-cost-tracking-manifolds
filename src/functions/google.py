74c53a6135fbd08ae0ea1a64cf51cec4019138b5
DmitriyAlergant-T1A <dmitriy.alergant@t1a.com>
2024-12-20 00:19:19 -0500

    New gemini models

--- src/functions/google.py
+++ src/functions/google.py
@@ -49,8 +49,20 @@ class Pipe:
                 "name": "gemini-1.5-flash",
             },
             {
+                "id": "gemini-2.0-flash-exp",
+                "name": "gemini-2.0-flash-exp",
+            },
+            {
+                "id": "gemini-2.0-flash-thinking-exp",
+                "name": "gemini-2.0-flash-thinking-exp",
+            },
+            {
                 "id": "gemini-1.5-pro",
                 "name": "gemini-1.5-pro",
+            },
+            {
+                "id": "gemini-exp-1206",
+                "name": "gemini-exp-1206",
             },
         ]
 
